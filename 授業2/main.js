9c19d850d0f15e0812ceab9dc345f9d001c5f1d0
cormo <ukai.persona@gmail.com>
2026-06-10 15:21:26 +0900

    Delete 授業2 directory

--- 授業2/main.js
+++
@@ -1,9 +0,0 @@
-import { changeBgColor, toggleTextSize } from './styleHelper.js';
-
-document.getElementById("bgBtn").addEventListener("click", () => {
-    changeBgColor("lightblue");
-});
-
-document.getElementById("fontBtn").addEventListener("click", () => {
-    toggleTextSize();
-});
